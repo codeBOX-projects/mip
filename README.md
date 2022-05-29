7cc59b081d722f40302393dfdd73596f25e0f7c3
codeBOX-projects <andrei.abd.codebox@gmail.com>
2022-05-29 09:03:30 -0400

    [code]BOX

---
+++ README.md
@@ -0,0 +1,18 @@
+# **MIP**
+> ## Introduction :
+### Get your own public ip address using site: [CHECK IP](http://checkip.dyndns.org/).
+---
+> ## Install & Usage :
+### **| clone repository :**
+```git clone https://github.com/codeBOX-projects/mip```
+### **| enter directory mip**
+```cd mip```
+### **| lunch by :**
+```python mip.py```
+
+---
+> ## Support distributions :
+- **Linux OS .**
+- **Termux .**
+---
+***[code]Box*** | ***Andrei A. Abd*** | ***1993-2022***
---
+++ mip.py
@@ -0,0 +1,29 @@
+import subprocess, termcolor, argparse
+
+parser = argparse.ArgumentParser(description="[?] Usage: "+ termcolor.colored("python3 public_ip.py", 'green'))
+args = parser.parse_args()
+
+info = """
+▒█▀▄▀█ █░░█ 　 ▀█▀ ▒█▀▀█ 
+▒█▒█▒█ █▄▄█ 　 ▒█░ ▒█▄▄█ 
+▒█░░▒█ ▄▄▄█ 　 ▄█▄ ▒█░░░ 
+\n
+[*] By Anonymose - find own Public IP Adress.
+[*] Get your public ip address using site: http://checkip.dyndns.org/.
+"""
+if __name__ == "__main__":
+    try:
+        #variable stored shell command...
+        shell_command = "curl -s http://checkip.dyndns.org/ | grep -o '[[:digit:].]\+'"
+        
+        #start program...
+        subprocess.call('clear', shell=True) #clear output terminal screen
+        print(info)
+        print(termcolor.colored("\n> Personal Public IP Address:", 'white'))
+        subprocess.call(shell_command, shell=True)
+    
+    except:
+        print(termcolor.colored("[!] Error: please restart the program"))
+
+
+
